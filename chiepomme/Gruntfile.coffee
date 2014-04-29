f554ae4f0537edccf2c0f26e09273c16036cd922
chiepomme <chie@loves-miko.cc>
2014-04-29 23:12:14 +0900

    initial commit

---
+++ chiepomme/Gruntfile.coffee
@@ -0,0 +1,41 @@
+﻿module.exports = (grunt) ->
+  grunt.file.mkdir "dist/"
+  grunt.initConfig
+    pkg : grunt.file.readJSON "package.json"
+
+    sass :
+      dist :
+        files :
+          "src/style.css" : "src/style.scss"
+
+    typescript :
+      base :
+        src : ["src/**/*.ts", "typings/**/*.d.ts"]
+
+    imagemin :
+      static :
+        files :
+          "src/logo.png" : "res/logo.png"
+          "src/favicon.png" : "res/favicon.png"
+
+    inline :
+      dist :
+        options :
+          cssmin : true
+          uglify : true
+        src : ["src/index.html"]
+        dest : ["dist/"]
+
+    watch :
+      files : ["src/**/*.html", "src/**/*.scss", "src/**/*.ts"]
+      tasks : ["default"]
+
+    grunt.loadNpmTasks "grunt-inline"
+    grunt.loadNpmTasks "grunt-typescript"
+    grunt.loadNpmTasks "grunt-contrib-sass"
+    grunt.loadNpmTasks "grunt-contrib-watch"
+    grunt.loadNpmTasks "grunt-contrib-uglify"
+    grunt.loadNpmTasks "grunt-contrib-cssmin"
+    grunt.loadNpmTasks "grunt-contrib-imagemin"
+
+    grunt.registerTask "default", ["typescript", "sass", "imagemin", "inline"]
---
+++ chiepomme/src/app.ts
@@ -0,0 +1,57 @@
+﻿class Chiepomme {
+
+    two: Two;
+    rect: Two.Shape;
+
+    start(root: HTMLElement) {
+        var params = {
+            type: Two.Types.canvas,
+            fullscreen: true,
+        };
+
+        this.two = new Two(params);
+        this.two.appendTo(root);
+
+        var circle = this.two.makeCircle(10, 10, 10);
+        this.rect = this.two.makeRectangle(30, 30, 30, 30);
+        circle.fill = "#FF8000";
+        circle.stroke = "orangered";
+        circle.linewidth = 5;
+
+        this.rect.fill = "#00CCFF";
+        this.rect.opacity = 0.75;
+        this.rect.noStroke();
+
+        this.rect.translation.set(20, 50);
+        this.rect.rotation = Math.PI;
+
+        this.two.bind(Two.Events.update, (frame) => {
+            console.log(frame);
+            this.rect.scale *= 1.001;
+        });
+
+        /*
+        window.onresize = (ev) => {
+            this.two.width = window.innerWidth;
+            this.two.height = window.innerHeight;
+        };
+        */
+
+        this.two.play();
+    }
+
+    update() {
+        /*
+        this.rect.translation.x += 1;
+        this.two.update();
+        window.setTimeout(this.update, 1);
+*/
+    }
+}
+
+window.onload = () => {
+    var chiepomme = new Chiepomme();
+    var el = document.getElementById("root");
+    var canvas = document.getElementsByTagName("canvas");
+    chiepomme.start(el);
+}
